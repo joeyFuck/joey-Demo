fd72fe14f4dd09c82210a61f0cb03c874f37e071
竹子 <747161601@qq.com>
2019-02-15 21:21:48 +0800

    RabbitMQ的改造封装，及IDisposable的合理配置使用，结合GC与析构函数

--- ConsoleCoreRabbitMQ/Program.cs
+++ ConsoleCoreRabbitMQ/Program.cs
@@ -1,5 +1,6 @@
 ﻿using RabbitMQ.Client;
 using System;
+using System.Collections.Generic;
 using System.Text;
 
 namespace ConsoleCoreRabbitMQ
@@ -14,12 +15,50 @@ namespace ConsoleCoreRabbitMQ
         static void Main(string[] args)
         {
             Console.WriteLine("hello rabbitMQ /producer");
+            TestMyRabbitMQHelper();
             //DefaultExchange();
             //DirectExchange();
             //FanoutExchange();
             //TopicExchange();
             Console.ReadKey();
         }
+
+        static void TestMyRabbitMQHelper()
+        {
+            using (RabbitMQHelper rm = new RabbitMQHelper(_exchangeName: "joey.amq.fanout"))
+            {
+                var channel = rm.GetChannel("joey");
+                var input = new List<InputModel> { new InputModel { id = "123", name = "Ed Sheeran" } };
+                rm.SendMsg<List<InputModel>>(channel, input);
+                Console.WriteLine("已成功发送：" + Newtonsoft.Json.JsonConvert.SerializeObject(input));
+            }
+            GC.Collect();
+            //由于rm已经手动释放资源且在dispose中GC.SuppressFinalize(this);通知垃圾回收器不再调用终结器。
+            //断点不会进入到析构函数中。
+
+
+            //var action = new Action(()=> {
+            //    RabbitMQHelper rm = new RabbitMQHelper(_exchangeName: "joey.amq.fanout");
+            //    var channel = rm.GetChannel("joey");
+            //    var input = new List<InputModel> { new InputModel { id = "123", name = "Ed Sheeran" } };
+            //    rm.SendMsg<List<InputModel>>(channel, input);
+            //    Console.WriteLine("已成功发送：" + Newtonsoft.Json.JsonConvert.SerializeObject(input));
+            //}); 
+            //action();
+
+            //GC.Collect();//手动调用GC，会调用所有即将回收的对象的析构函数。（虽然对象rm已经出了作用域被当做垃圾扔进了第0代，但是还不会进行垃圾回收）
+
+            //满足下面条件之一时，GC自动调用
+            //1 系统具有较低的物理内存；
+
+            //2 由托管堆上已分配的对象使用的内存超出了可接受的范围；
+
+            //3 手动调用GC.Collect方法，但几乎所有的情况下，我们都不必调用，因为垃圾回收器会自动调用它，但在上面的例子中，为了体验一下不及时回收垃圾带来的危害，所以手动调用了GC.Collect，大家也可以仔细体会一下运行这个方法带来的不同。
+
+            /*代的概念：一共分3代：0代、1代、2代。而这三代，相当于是三个队列容器，第0代包含的是一些短期生存的对象，上面的例子fs就是个短期对象，当方法执行完后，fs就被丢到了GC的第0代，但不进行垃圾回收，只有当第0代满了的时候，GC才会工作对其进行回收。*/
+        }
+
+
         /// <summary>
         /// 默认的Exchanges为name:(AMQP default) type:direct
         /// </summary>
@@ -86,6 +125,7 @@ namespace ConsoleCoreRabbitMQ
             channel.ExchangeDeclare(exchangeName, ExchangeType.Direct, false, false, null);
             //声明一个队列
             channel.QueueDeclare(queueName, false, false, false, null);//也可在管理台中手动添加queue http://localhost:15672/#/queues
+            channel.QueueDeclare(queueName2, false, false, false, null);
             //将队列绑定到交换机 add
             channel.QueueBind(queueName, exchangeName, routeKey, null);
             channel.QueueBind(queueName2, exchangeName, routeKey2, null);
@@ -135,10 +175,11 @@ namespace ConsoleCoreRabbitMQ
             var connection = factory.CreateConnection();
             //创建通道
             var channel = connection.CreateModel();
-            //定义一个Direct类型交换机 add 
+            //定义一个Fanout类型交换机 add 
             channel.ExchangeDeclare(exchangeName, ExchangeType.Fanout, false, false, null);
             //声明一个队列
             channel.QueueDeclare(queueName, false, false, false, null);//也可在管理台中手动添加queue http://localhost:15672/#/queues
+            channel.QueueDeclare(queueName2, false, false, false, null);
             //将队列绑定到交换机 add 绑定两个queue
             channel.QueueBind(queueName, exchangeName, routeKey, null);
             channel.QueueBind(queueName2, exchangeName, routeKey2, null);
@@ -188,10 +229,11 @@ namespace ConsoleCoreRabbitMQ
             var connection = factory.CreateConnection();
             //创建通道
             var channel = connection.CreateModel();
-            //定义一个Direct类型交换机 add 
+            //定义一个Topic类型交换机 add 
             channel.ExchangeDeclare(exchangeName, ExchangeType.Topic, false, false, null);
             //声明一个队列
             channel.QueueDeclare(queueName, false, false, false, null);//也可在管理台中手动添加queue http://localhost:15672/#/queues
+            channel.QueueDeclare(queueName2, false, false, false, null);
             //将队列绑定到交换机 add 绑定两个queue
             channel.QueueBind(queueName, exchangeName, routeKey, null);
             channel.QueueBind(queueName2, exchangeName, routeKey2, null);
@@ -211,5 +253,11 @@ namespace ConsoleCoreRabbitMQ
             connection.Close();
         }
 
+    }
+
+    class InputModel
+    {
+        public string id { get; set; }
+        public string name { get; set; }
     }
 }
---
+++ ConsoleCoreRabbitMQ/RabbitMQHelper.cs
@@ -0,0 +1,147 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleCoreRabbitMQ
+{
+    public class RabbitMQHelper:IDisposable
+    {
+        private ConnectionFactory factory;//连接工厂
+        private IConnection connection;//创建连接
+        private IModel channel;//创建通道
+        private string exchangeName;
+        private string routeKey; 
+         
+        private RabbitMQHelper() { }
+
+        public RabbitMQHelper(string _exchangeName,string _routeKey = "")
+        { 
+            this.factory = new ConnectionFactory
+            {
+                UserName = "guest",//用户名
+                Password = "guest",//密码
+                HostName = "192.168.101.81"//rabbitmq ip
+            };
+
+            this.connection = this.factory.CreateConnection();
+            this.channel = this.connection.CreateModel();
+            this.exchangeName = _exchangeName;
+            this.routeKey = _routeKey; 
+        }
+ 
+        /// <summary>
+        /// 设置Channel并返回
+        /// </summary>
+        /// <param name="queueName"></param>
+        /// <param name="type">ExchangeType</param>
+        /// <returns></returns>
+        public IModel GetChannel(string queueName, string type = ExchangeType.Fanout)
+        {
+            //定义一个Direct类型交换机 add 
+            channel.ExchangeDeclare(exchangeName, type, false, false, null); 
+            //声明一个队列
+            channel.QueueDeclare(queueName, false, false, false, null);
+            //将队列绑定到交换机 add 绑定两个queue
+            channel.QueueBind(queueName, exchangeName, routeKey, null);
+  
+            return channel; 
+        }
+        /// <summary>
+        /// 发送消息
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="_channel">通道实例 通过GetChannel获取</param>
+        /// <param name="input">消息主体对象</param>
+        public void SendMsg<T>(IModel _channel,T input) where T:class
+        {
+            var inputStr = Newtonsoft.Json.JsonConvert.SerializeObject(input);
+            var sendBytes = Encoding.UTF8.GetBytes(inputStr);
+            //发布消息
+            _channel.BasicPublish(exchangeName, routeKey, null, sendBytes);
+        }
+
+        #region 资源释放
+         
+        /// <summary>
+        /// 释放标记
+        /// </summary>
+        private bool disposed;
+
+        /// <summary>
+        /// 为了防止忘记显式的调用Dispose方法
+        /// </summary>
+        ~RabbitMQHelper()
+        {
+            //必须为false，到析构函数了说明已经由GC处理资源释放了，此时只需要手动释放非托管资源即可
+            Dispose(false);
+        }
+         
+        /// <summary>
+        /// 执行与释放或重置非托管资源关联的应用程序定义的任务。
+        /// </summary>
+        public void Dispose()
+        {
+            //必须为true
+            Dispose(true);
+            //通知垃圾回收器不再调用终结器 以防止垃圾回收器对不需要终止的对象调用 Object.Finalize。
+            GC.SuppressFinalize(this);
+        }
+
+        /// <summary>
+        /// 非必需的，只是为了更符合其他语言的规范，如C++、java
+        /// </summary>
+        public void Close()
+        {
+            Dispose();
+        }
+
+        /// <summary>
+        /// 非密封类可重写的Dispose方法，方便子类继承时可重写
+        /// </summary>
+        /// <param name="disposing">bool参数的目的，是为了释放资源时区分对待托管资源和非托管资源
+        /// 当传入true时代表要同时处理托管资源和非托管资源</param>
+        ///
+        /// 如果显式调用Dispose，那么类型就该按部就班的将自己的资源全部释放，
+        /// 如果忘记了调用Dispose，那就假定自己的所有资源(哪怕是非普通类型)都交给GC了，所以不需要手动清理托管资源，只需要释放非托管资源。
+        protected virtual void Dispose(bool disposing)
+        {
+            if (this.disposed)
+            {
+                return;
+            }
+            //清理托管资源
+            if (disposing)
+            {
+                if (this.factory != null)
+                {
+                    this.factory = null;
+                }
+                if (this.exchangeName != null)
+                {
+                    this.exchangeName = null;
+                }
+                if (this.routeKey != null)
+                {
+                    this.routeKey = null;
+                } 
+            }
+            //清理非托管资源
+            if (this.channel != null)
+            {
+                this.channel.Close();
+                this.channel = null;
+            }
+            if (this.connection != null)
+            { 
+                this.connection.Close();
+                this.connection = null;
+            }
+
+            //告诉自己已经被释放
+            this.disposed = true;
+        }
+
+        #endregion 
+    }
+}
